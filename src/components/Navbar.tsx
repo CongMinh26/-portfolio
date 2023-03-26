3d3324ec2b61acdd95c4bc1f7eca90dc77ca6e95
MinhNC <congminhad1999@gmail.com>
2023-03-26 23:26:24 +0700

    [M+] Commit hero

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,8 +8,8 @@ const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 top-0 z-20`}>
-      <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 top-0 z-20 fixed bg-primary`}>
+      <div className='w-full flex justify-between items-center max-w-7xl mx-auto '>
         <Link to='/'
           className='flex items-center gap-2'
           onClick={()=>{
